2d63dadf7a64aa21f1b439fea2e750b14fa8601e
Chris Kimpton <chris@kimptoc.net>
2011-04-27 21:33:12 +0100

    hardcoded for way my service needs it :(

--- lib/savon/core_ext/time.rb
+++ lib/savon/core_ext/time.rb
@@ -4,15 +4,7 @@ module Savon
 
       # Returns an xs:dateTime formatted String.
       def xs_datetime
-        zone = if utc_offset < 0
-          "-#{"%02d" % (- utc_offset / 3600)}:#{"%02d" % ((- utc_offset % 3600) / 60)}"
-        elsif utc_offset > 0
-          "+#{"%02d" % (utc_offset / 3600)}:#{"%02d" % ((utc_offset % 3600) / 60)}"
-        else
-          "Z"
-        end
-
-        strftime "%Y-%m-%dT%H:%M:%S#{zone}"
+        utc.strftime "%Y-%m-%dT%H:%M:%SZ"
       end
 
     end
