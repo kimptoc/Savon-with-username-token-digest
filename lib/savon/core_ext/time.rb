9c11b16c204bd97e8d64bd913122aabc72df7c8c
Chris Kimpton <chris@kimptoc.net>
2011-04-27 20:16:10 +0100

    force time to be utc

--- lib/savon/core_ext/time.rb
+++ lib/savon/core_ext/time.rb
@@ -4,7 +4,7 @@ module Savon
 
       # Returns an xs:dateTime formatted String.
       def xs_datetime
-        strftime "%Y-%m-%dT%H:%M:%S%Z"
+        utc.strftime "%Y-%m-%dT%H:%M:%S%Z"
       end
 
     end
